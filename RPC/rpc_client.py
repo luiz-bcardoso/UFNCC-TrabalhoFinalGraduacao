7d730297416a0cf56783c2a388ed137b258fb293
Luiz <luizbatistacardoso@gmail.com>
2024-11-21 16:27:37 -0300

    Update client and server code

--- RPC/rpc_client.py
+++ RPC/rpc_client.py
@@ -1,8 +1,16 @@
 import xmlrpc.client
 
 # Conecta no servidor local
-rpc_server = xmlrpc.client.ServerProxy("http://localhost:8000/")
+rpc_server = xmlrpc.client.ServerProxy(
+    "http://localhost:8000/"
+)
 
 # Faz a chamada remota do méotodo (RPC)
 resposta = rpc_server.soma(5, 3)
-print(f"Servidor retornou a seguinte mensagem: {resposta}")
+print(f"Servidor retornou a mensagem: {resposta}")
+
+
+
+
+
+
--- RPC/rpc_server.py
+++ RPC/rpc_server.py
@@ -6,10 +6,11 @@ def soma(x, y):
 
 # Cria o servidor RPC
 server = SimpleXMLRPCServer(("localhost", 8000))
-print("Servidor criado e escutando na porta 8000...")
+print("Servidor escutando na porta 8000...")
 
 # Registra as funções
 server.register_function(soma, "soma")
 
 # Começa a rodar o servidor indefinidamente
 server.serve_forever()
+
